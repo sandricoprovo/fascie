d2c05004281c4c1e7b243c8b376849b161fa495a
Sandrico Provo <Sprovo@outlook.com>
2022-03-18 08:15:23 -0300

    [chore]: Adds base files for the front-end.

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,6 @@ const server = express();
 
 server.use(express.json());
 
-server.listen(3000, () =>
-    console.log('Server ready at: http://localhost:3000')
+server.listen(4000, () =>
+    console.log('Server ready at: http://localhost:4000')
 );
